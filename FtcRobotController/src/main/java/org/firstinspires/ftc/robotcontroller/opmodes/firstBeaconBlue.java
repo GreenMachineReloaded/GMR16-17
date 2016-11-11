0a4a5d0dccdafe09cbece86907a06f47d1783150
noah <motherbennett@gmail.com>
2016-11-11 10:55:34 -0500

    made first beacon blue auto program

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/opmodes/firstBeaconBlue.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.robotcontroller.opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcontroller.GMRDriveCode.Directions;
+import org.firstinspires.ftc.robotcontroller.GMRDriveCode.Hardwaresetup;
+import org.firstinspires.ftc.robotcontroller.GMRDriveCode.MoveMotors;
+import org.firstinspires.ftc.robotcontroller.SensorObjects.ColorSensors;
+import org.firstinspires.ftc.robotcontroller.otherObjects.Continue;
+
+@Autonomous(name="hit beacon red one")
+public class firstBeaconBlue extends LinearOpMode{
+    public void runOpMode() throws InterruptedException {
+        Hardwaresetup set = new Hardwaresetup();
+        set.init(hardwareMap);
+        MoveMotors move = new MoveMotors();
+        move.init(hardwareMap);
+        Continue c = new Continue();
+        waitForStart();
+        move.colorWhiteDrive(Directions.DLeftUp, 50, ColorSensors.whichColorSensor.GROUNDLEFT);
+        move.Stop();
+        c.Sleep(100);
+        move.ProxDrive(Directions.Forward, 25, .7);
+        c.Sleep(100);
+        move.colorDriveRedBlue(Directions.StrafeRight, 50, ColorSensors.whichColorSensor.BEACON, ColorSensors.whichColor.BLUE);
+        move.Stop();
+        c.Sleep(100);
+        move.Drive(Directions.Forward, 25);
+        c.Sleep(300);
+        move.Stop();
+    }
+}
