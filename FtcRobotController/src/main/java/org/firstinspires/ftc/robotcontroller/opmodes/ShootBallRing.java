8090689d48c45600ae56fcf4450bdcd1a214c099
noah <motherbennett@gmail.com>
2016-11-17 22:25:02 -0500

    added Shoot Ball Ring

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/opmodes/ShootBallRing.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.robotcontroller.opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcontroller.GMRDriveCode.Directions;
+import org.firstinspires.ftc.robotcontroller.GMRDriveCode.Hardwaresetup;
+import org.firstinspires.ftc.robotcontroller.GMRDriveCode.MoveMotors;
+import org.firstinspires.ftc.robotcontroller.SensorObjects.ColorSensors;
+import org.firstinspires.ftc.robotcontroller.otherObjects.Continue;
+
+@Autonomous(name="lanch")
+
+public class ShootBallRing extends LinearOpMode{
+    public void runOpMode() throws InterruptedException {
+        Hardwaresetup set = new Hardwaresetup();
+        set.init(hardwareMap);
+        MoveMotors move = new MoveMotors();
+        move.init(hardwareMap, telemetry);
+        Continue c = new Continue();
+        DcMotor fire = hardwareMap.dcMotor.get("launchmotor");
+        waitForStart();
+        fire.setPower(.5);
+        c.Sleep(1000);
+        fire.setPower(0);
+    }
+}
