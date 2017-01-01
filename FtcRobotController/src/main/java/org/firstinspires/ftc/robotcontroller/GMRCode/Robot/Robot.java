b6b4694a728b9b19ee0dda4c10eed711de581742
Andor16 <pstoney16@gmail.com>
2017-01-01 18:14:37 -0500

    Added TeleOp program for new software setup, and have started fixing the null pointer issue.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/GMRCode/Robot/Robot.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/GMRCode/Robot/Robot.java
@@ -7,17 +7,18 @@ import org.firstinspires.ftc.robotcontroller.GMRCode.Robot.BaseClasses.WaitFor;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Robot {
+
     public DriveTrain driveTrain;
     public Launch launch;
     public BeaconNav beaconNav;
     public WaitFor waitFor;
-    public Robot(HardwareMap hardwareMap, Telemetry Telemetry) {new Robot(hardwareMap, Telemetry, false, false);}
-    public Robot(HardwareMap hardwareMap, Telemetry Telemetry, boolean writeDebug) {new Robot(hardwareMap, Telemetry, writeDebug, false);}
-    public Robot(HardwareMap hardwareMap, Telemetry Telemetry, boolean writeDebug, boolean writeToFile) {
-        Telemetry.addData("Robot Starting", "");
-        driveTrain = new DriveTrain(hardwareMap, Telemetry);
-        launch = new Launch(hardwareMap, Telemetry);
-        beaconNav = new BeaconNav(hardwareMap, Telemetry);
+
+    public Robot(HardwareMap hardwareMap, Telemetry telemetry) {new Robot(hardwareMap, telemetry, false, false);}
+    public Robot(HardwareMap hardwareMap, Telemetry telemetry, boolean writeDebug) {new Robot(hardwareMap, telemetry, writeDebug, false);}
+    public Robot(HardwareMap hardwareMap, Telemetry telemetry, boolean writeDebug, boolean writeToFile) {
+        this.driveTrain = new DriveTrain(hardwareMap, telemetry);
+        this.launch = new Launch(hardwareMap, telemetry);
+        this.beaconNav = new BeaconNav(hardwareMap, telemetry);
         waitFor = new WaitFor();
         if(writeDebug) {
             //start thread
@@ -25,6 +26,10 @@ public class Robot {
         if(writeToFile) {
             //start thread
         }
+        telemetry.addData("Robot Starting", "");
+        telemetry.addData("Drive Train", driveTrain);
+        telemetry.addData("Launch", launch);
+        telemetry.addData("Beacon Nav", beaconNav);
     }
 
     public boolean colorDrive(DriveTrain.Direction direction, double power, BeaconNav.WhichGMRColorSensor whichGMRColorSensor , BeaconNav.Color whichColor) {
@@ -55,5 +60,17 @@ public class Robot {
         driveTrain.Drive(direction, power);
         if(beaconNav.getRawDistance() > prox) {driveTrain.stop(); return true;}
         else {return false;}
+    }
+
+    public DriveTrain getDriveTrain() {
+        return driveTrain;
+    }
+
+    public Launch getLaunch() {
+        return launch;
+    }
+
+    public BeaconNav getBeaconNav() {
+        return beaconNav;
     }
 }
---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/GMRCode/TeleOp/MecanumTeleOp.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.robotcontroller.GMRCode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.GMRCode.Robot.BaseClasses.BeaconNav;
+import org.firstinspires.ftc.robotcontroller.GMRCode.Robot.BaseClasses.DriveTrain;
+import org.firstinspires.ftc.robotcontroller.GMRCode.Robot.BaseClasses.Launch;
+import org.firstinspires.ftc.robotcontroller.GMRCode.Robot.Robot;
+
+/**
+ * Created by Payton on 10/9/2016
+ */
+
+@TeleOp(name="Mecanum Drive: Version Four", group="Mecanum Bot")
+public class MecanumTeleOp extends OpMode {
+
+    private Robot robot;
+
+    private BeaconNav beaconNav;
+    private DriveTrain driveTrain;
+    private Launch launch;
+
+    private double x;
+    private double y;
+    private double z;
+
+    private ElapsedTime time = new ElapsedTime();
+
+    @Override
+    public void init() {
+        robot = new Robot(hardwareMap, telemetry);
+        beaconNav = robot.getBeaconNav();
+        driveTrain = robot.getDriveTrain();
+        launch = robot.getLaunch();
+        time.reset();
+    }
+
+    @Override
+    public void start() {
+        time.reset();
+        //robot.launch.liftControl(false, true);
+    }
+
+    @Override
+    public void loop() {
+        x = gamepad1.left_stick_x;
+        y = -gamepad1.left_stick_y;
+        z = gamepad1.right_stick_x;
+
+        //robot.driveTrain.setMotorPower(x, y, z);
+        //robot.launch.launchControl(gamepad1.left_bumper, gamepad1.x);
+        //robot.launch.sweeperControl(gamepad1.right_bumper, gamepad1.right_trigger);
+        //robot.launch.linearSlideControl(gamepad2.a, gamepad2.b);
+        //robot.launch.liftControl(gamepad2.dpad_up, gamepad2.dpad_down);
+        updateTelemetry(telemetry);
+
+        telemetry.addData("Current time", time.seconds());
+        //telemetry.addData("Current Yaw", robot.driveTrain.getYaw());
+
+        telemetry.addData("Robot Value", robot);
+        telemetry.addData("Robot Class", driveTrain.getClass().getDeclaredMethods());
+
+    }
+}
