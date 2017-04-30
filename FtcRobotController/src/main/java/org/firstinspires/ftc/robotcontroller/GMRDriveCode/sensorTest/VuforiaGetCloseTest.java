5e975f14b2c4110dd9844d63f4864354b302d126
sixthkid <moogie0203@gmail.com>
2017-04-30 01:54:01 -0400

    updated android studios

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/GMRDriveCode/sensorTest/VuforiaGetCloseTest.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/GMRDriveCode/sensorTest/VuforiaGetCloseTest.java
@@ -1,8 +1,19 @@
 package org.firstinspires.ftc.robotcontroller.GMRDriveCode.sensorTest;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.robotcontroller.GMRCode.Robot.Robot;
+import org.firstinspires.ftc.robotcontroller.GMRCode.otherObjects.CurrentStates;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+@Autonomous(name="get close test", group="Tests")
+public class VuforiaGetCloseTest extends OpMode {
+    private Robot robot;
+    public void init() {
 
-/**
- * Created by Noah Bennett on 4/23/2017.
- */
-
-public class VuforiaGetCloseTest {
+    }
+    public void start() {robot = new Robot(hardwareMap, telemetry);}
+    public void loop() {
+        robot.waitFor.Sleep(100);
+        telemetry.addData("getArrayXYZ", robot.robotEyes.getArrayXYZ());
+        telemetry.addData("Current Visual", robot.robotEyes.getImageOfCurrentVisualBeacon());
+    }
 }
